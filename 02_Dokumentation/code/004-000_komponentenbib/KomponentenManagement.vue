5f15f78b7d7efbd8aa56202dcf20ab061e4bee3e
Marvin Klimm <github@marvin.works>
2020-11-26 18:48:56 +0100

    Better Componentmanagement

---
+++ 02_Dokumentation/code/004-000_komponentenbib/KomponentenManagement.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav>
+    <navbar-tab v-show="showTab" />
+    <navbar-tab-button @click="showTab = !showTab">Toggle</nav-tab-button>
+  </nav>
+</template>
+
+<script>
+import NavbarTab from './_components/NavbarTab.vue'
+import NavbarTabButton from './_components/NavbarTabButton.vue'
+
+export default {
+  data() {
+    return {
+      showTab: false
+    }
+  },
+  components: {
+    NavbarTab,
+    NavbarTabButton
+  }
+}
+</script>
