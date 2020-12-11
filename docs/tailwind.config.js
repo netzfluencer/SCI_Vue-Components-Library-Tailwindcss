c12b2298840113200cb294f97ea915543f872656
Marvin Klimm <github@marvin.works>
2020-12-11 10:23:04 +0100

    renamed reveal.js dir to docs

---
+++ docs/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        mi: {
+          lila: '#9829C8'
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
